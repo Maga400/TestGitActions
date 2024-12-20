47f1dabba79c045522aa534eb03fad2017a43ed4
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2024-12-20 11:05:45 +0400

    ccc

--- TestGitActions/Controllers/HomeController.cs
+++ TestGitActions/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace TestGitActions.Controllers
         public IActionResult Index()
         {
             var name = "Elvin";
-            var sss = "SSS"
+            var sss = "SSS";
             return View();
         }
 
